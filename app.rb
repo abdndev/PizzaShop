dde54fa05a033506f39132a601c1d5b1be8c8200
ABDNdev <igor@abidin.ru>
2019-11-30 18:08:31 +0300

    Add Orders model + migration

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ set :database, "sqlite3:pizzashop.db"
 class Product < ActiveRecord::Base
 end
 
+class Order < ActiveRecord::Base
+end
+
 get '/' do
 	@products = Product.all 
 	erb :index			
@@ -56,3 +59,4 @@ def parse_orders_input orders_input
 
 	return arr
 end
+
