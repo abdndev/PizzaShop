fd16bebe2d6a84d46cb85d53e8f0af6659f9dcc8
ABDNdev <igor@abidin.ru>
2019-12-01 19:48:23 +0300

    Replace cart contents on cancel

--- public/app.js
+++ public/app.js
@@ -78,7 +78,11 @@ function cart_get_orders()
 function cancel_order() 
 {
 	window.localStorage.clear();
+
 	update_orders_input();
 	update_orders_button();
+
+	$('#cart').text('Your cart is now empty');
+
 	return false;
 }
